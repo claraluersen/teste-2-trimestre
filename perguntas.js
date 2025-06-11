04fce7a5232e9570992c2cd4392c2fbd52e58526
claraluersen <clara.luersen@escola.pr.gov.br>
2025-06-11 14:19:03 +0000

    kh

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Basicas',
+    'quantas cores tem no arco-híris?',
+    'O arco-híris tem 7 cores'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior planeta do sistema solar?',
+    'O maior planeta é Jupiter'
+)
+
+criaCartao(
+    'Programação',
+    '?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
